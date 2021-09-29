df5a1a985b4ad73784271ad775be92db0c504cca
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-30 00:36:53 +0300

    removed all imgs and changed layout

--- components/Utils/ProjectCard.jsx
+++ components/Utils/ProjectCard.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import styles from "../Utils/ProjectCard.module.css";
 import Image from "next/image";
-import { image3 } from "../../public/images/3.jpg";
 
 function ProjectCard() {
   return (
     <div className={styles.projectCard}>
-      <Image src={image3} className={styles.projectCard__img} />
       <div className={styles.projectCard__title}>Title</div>
       <div className={styles.projectCard__divider}></div>
       <br />
